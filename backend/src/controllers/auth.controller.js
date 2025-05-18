3f7045a98362c4245e5160d528c76da12213776f
Mohit <mohitswarnkar13@gmail.com>
2025-05-18 05:49:07 +0530

    cloudinary

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs"
+import cloudinary from "../lib/cloudinary.js"
 
 export const signup =async (req,res)=>{
     const {fullName,email,password}=req.body
@@ -80,5 +81,23 @@ export const logout = (req,res)=>{
     catch(error){
         console.log("Error in Logout Controller",error.message);
         res.status(500).json({message:"Internal Server Error"});
+    }
+}
+
+export const updateProfile = async(req,res)=>{
+    try{
+        const {profilePic} = req.body;
+        const userId = req.user._id;
+        if(!profilePic){
+            return res.status(400).json({message:"Profile pic is required"});
+        }
+
+        const uploadResplonse = await cloudinary.uploader.upload(profilePic)
+        const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResplonse.secure_url},{new:true})
+
+        res.status(200).json(updateUser)
+    }catch(error){
+        console.log('error in update profile:',error)
+        res.status(500).json({message:"Internal server error"})
     }
 }
