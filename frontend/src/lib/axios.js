93908b3ab25399c91928c88b37deb559f265a6fb
Mohit <mohitswarnkar13@gmail.com>
2025-06-10 23:57:56 +0530

    final updates

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development"
-    ? "http://localhost:5001/api"
-    : "https://voxa-e6gs.onrender.com/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
 });
